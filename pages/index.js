9d495663af208f9d3000733b24a6dad54002cff7
Gabriel Mira <gabrielr01oficial@gmail.com>
2025-11-30 20:34:26 -0300

    add imagem

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,19 @@
-function Home(){
-    return <h1>Welcome to the Home Page! Change again</h1>;
+function Home() {
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            <h1>Estou estudando doutora adriana</h1>
+            <h3>Olha essa menina linda na foto</h3>
+            <img
+                src="/adrianita.jpeg" 
+                alt="Doutora Adriana"
+                height={700}
+            />
+        </div>
+    );
 }
 export default Home;
