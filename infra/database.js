4b4b5860b69217d026a102bbf181a8c2ba915cbc
Gabriel <gabrielr01oficial@gmail.com>
2026-01-02 23:25:48 -0300

    change `database.js` to accept ssl mode

--- infra/database.js
+++ infra/database.js
@@ -1,18 +1,13 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  console.log(`Ambiente selecionado: ${process.env.NODE_ENV}`);
-
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl:
-      process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   try {
     await client.connect();
